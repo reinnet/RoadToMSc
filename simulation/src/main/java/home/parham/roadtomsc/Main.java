d4529e800da418889af81dbba3012dfde95e6cf9
Parham Alvani <parham.alvani@gmail.com>
2018-08-26 11:16:56 +0430

    Adds placement model. #2.

--- simulation/src/main/java/home/parham/roadtomsc/Main.java
+++ simulation/src/main/java/home/parham/roadtomsc/Main.java
@@ -5,6 +5,7 @@ import home.parham.roadtomsc.domain.Node;
 import home.parham.roadtomsc.domain.Type;
 import ilog.concert.IloException;
 import ilog.concert.IloIntVar;
+import ilog.concert.IloLinearIntExpr;
 import ilog.concert.IloLinearNumExpr;
 import ilog.cplex.IloCplex;
 
@@ -23,9 +24,13 @@ public class Main {
         // number of SFC requests
         // consider to create requests after creating VNF types
         final Chain[] chains = {
-                new Chain(10).addVNF(0)
+                new Chain(10).addNode(0)
         };
         final int T = chains.length;
+        int V = 0; // Total number of VNF
+        for (Chain chain : chains) {
+            V += chain.nodes();
+        }
 
         // VNFM
         int nfvmRam = 1;
@@ -43,14 +48,19 @@ public class Main {
             }
             IloIntVar[] x = cplex.boolVarArray(T, xNames);
 
-            // the number of VNF instances of type _k_ that are used in server w
-            String[] yNames = new String[W * F];
+            // the number of VNF instances of type _k_ that are used in server _w_
+            String[][] yNames = new String[W][F];
             for (int i = 0; i < W; i++) {
                 for (int j = 0; j < F; j++) {
-                    yNames[i * F + j] = String.format("y_%d_%d", i, j);
+                    yNames[i][j] = String.format("y_%d_%d", i, j);
                 }
             }
-            IloIntVar[] y = cplex.intVarArray(W * F, 0, Integer.MAX_VALUE,  yNames);
+            IloIntVar[][] y = new IloIntVar[W][F];
+            for (int i = 0; i < W; i++) {
+                for (int j = 0; j < F; j++) {
+                    y[i][j] = cplex.intVar(0, Integer.MAX_VALUE, yNames[i][j]);
+                }
+            }
 
             // binary variable assuming the value 1 if VNFM on server w is used;
             // otherwise its value is zero
@@ -59,6 +69,29 @@ public class Main {
                 yHatNames[i] = String.format("y^_%d", i);
             }
             IloIntVar[] yHat = cplex.boolVarArray(W, yHatNames);
+
+            // binary variable assuming the value 1 if the VNF node _v_ is served by the VNF instance of type
+            // _k_ in the server _w_
+            String[][][] zNames = new String[F][W][V];
+            for (int i = 0; i < F; i++) {
+                for (int j = 0; j < W; j++) {
+                    int v = 0;
+                    for (int h = 0; h < T; h++) {
+                        for (int k = 0; k < chains[h].nodes(); k++) {
+                            zNames[i][j][k + v] = String.format("z_%d_%d_%d-%d", i, j, h, k);
+                        }
+                        v += chains[h].nodes();
+                    }
+                }
+            }
+            IloIntVar[][][] z = new IloIntVar[F][W][V];
+            for (int i = 0; i < F; i++) {
+                for (int j = 0; j < W; j++) {
+                    for (int k = 0; k < V; k++) {
+                        z[i][j][k] = cplex.boolVar(zNames[i][j][k]);
+                    }
+                }
+            }
 
             // objective function
             IloLinearNumExpr expr = cplex.linearNumExpr();
@@ -75,12 +108,42 @@ public class Main {
                 cpuConstraint.addTerm(nfvmCores, yHat[i]); // VNFM
 
                 for (int j = 0; j < F; j++) {
-                    ramConstraint.addTerm(Type.get(j).getRam(), y[i * F + j]); // instance
-                    cpuConstraint.addTerm(Type.get(j).getCores(), y[i * F + j]); // instance
+                    ramConstraint.addTerm(Type.get(j).getRam(), y[i][j]); // instance
+                    cpuConstraint.addTerm(Type.get(j).getCores(), y[i][j]); // instance
                 }
 
-                cplex.addGe(cpuConstraint, nodes[i].getRam(), String.format("node_cpu_constraint %d", i));
-                cplex.addGe(ramConstraint, nodes[i].getRam(), String.format("node_memory_constraint %d", i));
+                cplex.addLe(cpuConstraint, nodes[i].getRam(), String.format("node_cpu_constraint %d", i));
+                cplex.addLe(ramConstraint, nodes[i].getRam(), String.format("node_memory_constraint %d", i));
+            }
+
+            // Service Place Constraint
+            for (int i = 0; i < W; i++) {
+                for (int j = 0; j < F; j++) {
+                    IloLinearIntExpr constraint = cplex.linearIntExpr();
+
+                    for (int k = 0; k < V; k++) {
+                        constraint.addTerm(1, z[i][j][k]);
+                    }
+
+                    cplex.addLe(constraint, y[i][j], String.format("service_place_constraint %d %d", i, j));
+                }
+            }
+
+            // Service Constraint
+            int v = 0;
+            for (int h = 0; h < T; h++) {
+                for (int k = 0; k < chains[h].nodes(); k++) {
+                    IloLinearIntExpr constraint = cplex.linearIntExpr();
+
+                    for (int i = 0; i < W; i++) {
+                        for (int j = 0; j < F; j++) {
+                            constraint.addTerm(1, z[i][j][k + v]);
+                        }
+                    }
+
+                    cplex.addEq(constraint, x[h], String.format("service_constraint %d", h));
+                }
+                v += chains[h].nodes();
             }
 
 
