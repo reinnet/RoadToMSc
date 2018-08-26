a326d18abab36fbf637a019704bc68835a00035f
Parham Alvani <parham.alvani@gmail.com>
2018-08-26 14:12:10 +0430

    Adds links method for chains.

--- simulation/src/main/java/home/parham/roadtomsc/domain/Chain.java
+++ simulation/src/main/java/home/parham/roadtomsc/domain/Chain.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 public class Chain {
     private ArrayList<Type> chain;
+    private ArrayList<Link> links;
     private int cost;
 
     public Chain(int cost) {
         this.chain = new ArrayList<>();
+        this.links = new ArrayList<>();
         this.cost = cost;
     }
 
@@ -16,13 +18,24 @@ public class Chain {
         return this;
     }
 
+    public Chain addLink(int bandwith, int source, int desitnation) {
+        this.links.add(new Link(bandwith, source, desitnation));
+        return this;
+    }
+
     public Type getNode(int index) {
         return this.chain.get(index);
+    }
+
+    public Link getLink(int index) {
+        return this.links.get(index);
     }
 
     public int nodes() {
         return this.chain.size();
     }
+
+    public int links() {return this.links.size(); }
 
     public int getCost() {
         return cost;
