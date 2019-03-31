29ba059097d123c8926fc6e7718a40a076517fb8
Parham Alvani <parham.alvani@gmail.com>
2019-04-01 00:28:09 +0430

    Adds method for adding constraints of node's manager.

--- simulation/src/main/java/home/parham/roadtomsc/domain/Chain.java
+++ simulation/src/main/java/home/parham/roadtomsc/domain/Chain.java
@@ -5,16 +5,25 @@ import java.util.ArrayList;
 public class Chain {
     private ArrayList<Type> chain;
     private ArrayList<Link> links;
+
+    private ArrayList<ArrayList<Integer>> managementConstraints;
     private int cost;
 
     public Chain(int cost) {
         this.chain = new ArrayList<>();
         this.links = new ArrayList<>();
+        this.managementConstraints = new ArrayList<>();
         this.cost = cost;
     }
 
     public Chain addNode(int id) {
+        this.addNodeWithManagementConstraints(id, new ArrayList<Integer>());
+        return this;
+    }
+
+    public Chain addNodeWithManagementConstraints(int id, ArrayList<Integer> constraints) {
         this.chain.add(Type.get(id));
+        this.managementConstraints.add(constraints);
         return this;
     }
 
@@ -39,5 +48,9 @@ public class Chain {
 
     public int getCost() {
         return cost;
+    }
+
+    public ArrayList<ArrayList<Integer>> getManagementConstraints() {
+        return managementConstraints;
     }
 }
