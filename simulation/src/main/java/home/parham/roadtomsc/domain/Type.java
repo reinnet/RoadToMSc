ed1a89be330a9b9853f21e0fee8d663c21f6c213
Parham Alvani <parham.alvani@gmail.com>
2019-06-25 00:48:29 +0430

    feat: Add Egress/Ingress for Types

--- simulation/src/main/java/home/parham/roadtomsc/domain/Type.java
+++ simulation/src/main/java/home/parham/roadtomsc/domain/Type.java
@@ -23,10 +23,24 @@ public class Type {
      */
     private int index;
 
+    /**
+     * indicates that this type is an egress type so it must be placed only on egress nodes
+     */
+    private boolean egress;
+
+    /**
+     * indicates that is type is an ingress type so it must be placed only on ingress nodes
+     */
+    private boolean ingress;
+
     private static ArrayList<Type> types;
 
+    public static void add(int cores, int ram, boolean egress, boolean ingress) {
+        types.add(new Type(types.size(), cores, ram, egress, ingress));
+    }
+
     public static void add(int cores, int ram) {
-        types.add(new Type(types.size(), cores, ram));
+        types.add(new Type(types.size(), cores, ram, false, false));
     }
 
     public static Type get(int id) {
@@ -37,10 +51,12 @@ public class Type {
         return types.size();
     }
 
-    private Type(int index, int cores, int ram) {
+    private Type(int index, int cores, int ram, boolean ingress, boolean egress) {
         this.cores = cores;
         this.ram = ram;
         this.index = index;
+        this.egress = egress;
+        this.ingress = ingress;
     }
 
     public int getCores() {
@@ -53,5 +69,13 @@ public class Type {
 
     public int getIndex() {
         return index;
+    }
+
+    public boolean isIngress() {
+        return ingress;
+    }
+
+    public boolean isEgress() {
+        return egress;
     }
 }
